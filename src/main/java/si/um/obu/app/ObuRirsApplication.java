28e856ddf50a7d969cdd84a704c4565f6f9438a9
Matt Hahn <matt.hahn@matthahndesign.com>
2015-12-15 11:32:56 +0100

    First Commit

---
+++ src/main/java/si/um/obu/app/ObuRirsApplication.java
@@ -0,0 +1,12 @@
+package si.um.obu.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObuRirsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObuRirsApplication.class, args);
+    }
+}
