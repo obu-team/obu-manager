902aabae6b4ea9e4fa9526aa7cbe93ec1f68b7c0
Matt Hahn <matt.hahn@matthahndesign.com>
2016-01-08 15:29:28 +0100

    Current location drive & History update

--- src/main/resources/static/js/currentLocation.js
+++ src/main/resources/static/js/currentLocation.js
@@ -1,22 +1,22 @@
 
 $(function() {
 	var currentLocationContainer = $("#currentLocation");
-	/*var currentLocation = new Maplace({
-	    locations: [{
-	    	lat: 46.5605926,
-        lon: 15.6325071,
-        zoom: 18
-	    }],
-	    map_div: '#currentLocation',
-	    controls_on_map: false
-	});
-	currentLocation.Load();*/
 	var clPath = {};
 	var clName = '';
+	var toPath = null;
+	var toPathName = '';
 	async.series([
 		function(cb) {
 			$.get('/'+obuID+'/location',{},function(res) {
 				clPath = res;
+				cb();
+			});
+		},
+		function(cb) {
+			$.get('/'+obuID+'/destination',{},function(res) {
+				if(res && res.lat && res.lon) {
+					toPath = res;
+				}
 				cb();
 			});
 		},
@@ -27,21 +27,47 @@ $(function() {
 				}
 				cb();
 			});
+		},
+		function(cb) {
+			if(toPath) {
+				$.get('http://maps.googleapis.com/maps/api/geocode/json?latlng='+toPath.lat+','+toPath.lon+'&sensor=false',{},function(res) {
+					if(res.results.length) {
+						toPathName = res.results[0].formatted_address;
+					}
+					cb();
+				});
+			} else {
+				cb();
+			}
 		}
 	],function(err) {
 		currentLocationContainer.html('');
-		new Maplace({
+		var currentLocationMatOpts = {
 			locations: [{
 				lat: clPath.lat,
-				lon: clPath.lon,
-				zoom: 18
+				lon: clPath.lon
 			}],
-			map_div: '#currentLocation',
-			controls_on_map: false
-		}).Load();
-		if(clName.length) {
+			map_div: '#currentLocation'
+		};
+
+		if(toPath) {
+			currentLocationMatOpts.generate_controls = false;
+			currentLocationMatOpts.show_markers = false;
+			currentLocationMatOpts.type = 'directions';
+			currentLocationMatOpts.draggable = true;
+			currentLocationMatOpts.locations.push({
+				lat: toPath.lat,
+				lon: toPath.lon
+			});
+		} else {
+			currentLocationMatOpts.locations[0].zoom = 18;
+			currentLocationMatOpts.controls_on_map = false;
+		}
+
+		new Maplace(currentLocationMatOpts).Load();
+		if(clName.length && toPathName.length) {
 			var htmlNameFrom = encodeURI(clName);
-			var htmlNameTo = encodeURI('Ljubaljana, Slovenia');
+			var htmlNameTo = encodeURI(toPathName);
 			$(".card.iframe").slideDown(200,function() {
 				$(this).append('<iframe src="http://www.brianfolts.com/driver/#origin='+htmlNameFrom+'&amp;destination='+htmlNameTo+'&amp;fps=1&amp;travelmode=BICYCLING&amp;rn=" scrolling="no" id="driving"></iframe>');
 			});
